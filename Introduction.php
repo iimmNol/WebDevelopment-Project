fdf8c27e5ae8664cd776d785be07d6e830759246
TrickyDESI <bremtricky@gmail.com>
2024-10-11 01:43:02 +0800

    Update Introduction.php

--- Introduction.php
+++ Introduction.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Tutorials - Introduction to PHP</title>
-    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <!-- INTRODDUCTION TO PHP -->
