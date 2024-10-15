871a812378e8429d4ff2a9873d4cebe5ce683d23
iimmNol <167633221+iimmNol@users.noreply.github.com>
2024-10-16 00:21:33 +0800

    Course.php

--- Website/Course.php
+++ Website/Course.php
@@ -33,8 +33,13 @@
 
     <div class="course-grid">
       <div class="course-card">
-        <img src="img/Introduction.jpg" alt="Introduction to PHP"> 
+        <a href="Introduction.php">
+          <img src="img/Introduction.jpg" alt="Introduction to PHP"> 
+        </a>
+
+        <a href="Introduction.php">
           <h3>Introduction to PHP</h3>
+        </a>
 
           <label class="switch">
             <input type="checkbox" id="toggleSwitch" checked>
@@ -45,9 +50,14 @@
       </div>
 
       <div class="course-card">
+        <a href="BasicSyntax.php">
         <img src="img/BasicSyntax.png" alt="PHP Basic Syntax">
-        <h3>PHP Basic Syntax</h3>
+        </a>
 
+        <a href="BasicSyntax.php">
+          <h3>PHP Basic Syntax</h3>
+        </a>
+        
         <label class="switch">
             <input type="checkbox" id="toggleSwitch2" checked>
             <span class="slider round"></span>
@@ -57,8 +67,14 @@
       </div>
 
       <div class="course-card">
-        <img src="img/Conditional.png" alt="Conditional and Looping Statement">
-        <h3>Condition and Loop Statement</h3>
+        <a href="Condition.php">
+          <img src="img/Conditional.png" alt="Conditional and Looping Statement">
+        </a>
+
+        <a href="Condition.php">
+          <h3>Condition & Loop Statement</h3>
+        </a>
+
         <label class="switch">
             <input type="checkbox" id="toggleSwitch3" checked>
             <span class="slider round"></span>
@@ -68,8 +84,13 @@
       </div>
 
       <div class="course-card">
-        <img src="img/Arrays.webp" alt="Arrays and Superglobals">
-        <h3>Arrays and Superglobals</h3>
+        <a href="Arrays.php">
+          <img src="img/Arrays.webp" alt="Arrays and Superglobals">
+        </a>
+
+        <a href="Arrays.php">
+          <h3>Arrays and Superglobals</h3>
+        </a>
         <label class="switch">
             <input type="checkbox" id="toggleSwitch4" checked>
             <span class="slider round"></span>
@@ -79,8 +100,13 @@
       </div>
 
       <div class="course-card">
-        <img src="img/Classes.jfif" alt="Classes, Objects, and Forms">
-        <h3>Classes, Objects, and Forms</h3>
+        <a href="Classes.php">
+          <img src="img/Classes.jfif" alt="Classes, Objects, and Forms">
+        </a>
+
+        <a href="Classes.php">
+          <h3>Classes, Objects, and Forms</h3>
+        </a>
         <label class="switch">
             <input type="checkbox" id="toggleSwitch5" checked>
             <span class="slider round"></span>
@@ -90,8 +116,14 @@
       </div>
 
       <div class="course-card">
-        <img src="img/sql.png" alt="MySQL">
+        <a href="MySql.php">
+          <img src="img/sql.png" alt="MySQL">
+        </a>
+
+        <a href="MySql.php">
         <h3>MySQL</h3>
+        </a>
+
         <label class="switch">
             <input type="checkbox" id="toggleSwitch6" checked>
             <span class="slider round"></span>
